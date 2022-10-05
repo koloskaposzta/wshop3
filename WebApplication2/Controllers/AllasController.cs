2d93345f1aa4af2fbb854d6ff3c4f31497ae0937
kolos <hallgato@LABOR210-01>
2022-10-05 12:29:09 +0200

    aSD

---
+++ WebApplication2/Controllers/AllasController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Data;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class AllasController : Controller
+    {
+        private readonly UserManager<SiteUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        private readonly ApplicationDbContext _db;
+
+        public AllasController(UserManager<SiteUser> userManager, RoleManager<IdentityRole> roleManager, ApplicationDbContext db)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
