c0ffc6837f0acfafeec1681a8a928e27d8949d93
PBoldi <boldi333333@gmail.com>
2022-10-05 12:13:05 +0200

    Models

---
+++ WebApplication2/Models/Allas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace workshop03.Models
+{
+    public class Allas
+    {
+        [Key]
+        public string UID { get; set; }
+        public string Vallalat { get; set; }
+        public string Megnevezes { get; set; }
+        public int Oraber { get; set; }
+        public virtual ICollection<SiteUser> Jelentkezok { get; set; }
+    }
+}
---
+++ WebApplication2/Models/SiteUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace workshop03.Models
+{
+    public class SiteUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public int MinimumOraber { get; set; }
+
+        public virtual ICollection<Allas> Allasok { get; set; }
+    }
+}
